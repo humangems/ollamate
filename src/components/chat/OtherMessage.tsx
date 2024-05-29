739f7f002fbb4962bb1df8a227b3480fb1749dc9
Michael Chen <mechiland@gmail.com>
2024-05-29 12:17:51 +0800

    update ollma icon

--- src/components/chat/OtherMessage.tsx
+++ src/components/chat/OtherMessage.tsx
@@ -1,5 +1,5 @@
 import 'katex/dist/katex.min.css';
-import { LoaderIcon, RabbitIcon } from 'lucide-react';
+import { LoaderIcon } from 'lucide-react';
 import Markdown from 'react-markdown';
 import rehypeKatex from 'rehype-katex';
 import rehypeMathjax from 'rehype-mathjax';
@@ -7,6 +7,7 @@ import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 import { Message } from '../../lib/types';
 import { useAppSelector } from '../../redux/store';
+import OllamaIcon from '../icons/Ollama';
 
 
 export default function OtherMessage({ message }: { message: Message }) {
@@ -16,8 +17,10 @@ export default function OtherMessage({ message }: { message: Message }) {
     <div className="w-full py-4">
       <div className="flex items-start space-x-2">
         <div className="w-12 shrink-0 font-medium text-gray-11 text-2">
-          <div className="border size-8 rounded-full flex items-center justify-center">
-            <RabbitIcon className="size-5" />
+          <div className=" size-9 overhi bg-grayA-3 rounded-full flex items-center justify-center">
+            <div className='p-2'>
+              <OllamaIcon />
+            </div>
           </div>
         </div>
         <div className="prose flex-1">
