4fbe6dfd37572cc1474afa2cfd0daa78748cf6e3
Michael Chen <mechiland@gmail.com>
2024-06-18 16:07:47 +0800

    code clean up

--- src/components/chat/RenameForm.tsx
+++ src/components/chat/RenameForm.tsx
@@ -31,9 +31,7 @@ export default function RenameForm({ chatId }: { chatId: string }) {
   return (
     <form onSubmit={form.onSubmit(handleSubmit)} action="xxx">
       <div>
-        {/* TODO: Radix TextField.Root ref changed, the Mantine form component cannot change the ref. */}
         <TextField.Root className="w-full" {...form.getInputProps("title")} />
-        {/* <input type="text" className="shadow-1 w-full p-1 border-accent-4 focus:border-accent-4 outline-accent-6 rounded-2" {...form.getInputProps('title')} /> */}
       </div>
       <div className="flex items-center justify-end mt-2 space-x-3">
         <Button color="gray" variant="soft" onClick={handleCancel} type="button">
